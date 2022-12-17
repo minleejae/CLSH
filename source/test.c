4774b3198d3c8132ea8769f08c32ecb702e36046
minleejae <mmj9808@naver.com>
2022-12-18 00:54:01 +0900

    FEATURE : clsh 명령어 구현

--- source/test.c
+++ source/test.c
@@ -10,12 +10,11 @@
 
 #define MSGSIZE 300
 
-
-int connectedNodes = 0;
+int totalNodesNum = 4;
 char *totalNodes[] = {"node1", "node2", "node3", "node4"};
 
-//원격 명령어 파싱하는 함수
-void getRemoteCommend(char parsing[][100], int start, int inputWords, char remoteCommand[]) {
+//원격 명령어 파싱하는 함수, 문자 개수 리턴
+int getRemoteCommend(char parsing[][100], int start, int inputWords, char remoteCommand[]) {
     int idx = 0;
     for (int i = start; i < inputWords; i++) {
         for (int j = 0; parsing[i][j] != '\0'; j++) {
@@ -23,7 +22,9 @@ void getRemoteCommend(char parsing[][100], int start, int inputWords, char remot
         }
         remoteCommand[idx++] = ' ';
     }
-    remoteCommand[idx - 1] = '\0';
+    remoteCommand[idx++] = '\n';
+    remoteCommand[idx++] = '\0';
+    return idx;
 }
 
 
@@ -39,82 +40,19 @@ int parseInput(char buf[], char parsing[][100], const char *delimeter) {
 }
 
 
-int input() {
-    char buf[128];
-    memset(buf, 0, sizeof(buf));
-    fgets(buf, sizeof(buf) - 1, stdin);
-    buf[strlen(buf) - 1] = '\0';
-
-    if (!strncmp(buf, "exit", strlen(buf))) {
-        return -1;
-    }
-
-    //문자열 파싱
-    char parsing[10][100] = {0};
-    int inputWords = parseInput(buf, parsing, " ");
-
-    //clsh 명령어
-    if (!strncmp(parsing[0], "clsh", strlen(parsing[0]))) {
-        char nodes[10][100] = {0};
-        char remoteCommand[200] = {0};
-
-        //clsh -h node1,node2,node3,node4 cat /proc/loadavg
-        if (!strncmp(parsing[1], "-h", strlen(parsing[0]))) {
-            int nodesNum = parseInput(parsing[2], nodes, ",");
-
-            printf("nodes!\n");
-            for (int i = 0; i < nodesNum; i++) {
-                printf("%s\n", nodes[i]);
-
-                pid_t pid = fork();
-                if (pid < 0) {
-                    perror("fork error\n");
-                    return -1;
-                } else if (pid == 0) {
-                    printf("Child Process My PID:%d, My Parent's PID:%d, MY Childs'Pid %d\n", (int) getpid(),
-                           (int) getppid(),
-                           (int) pid);
-//                    char *exec_arg[] = {"ssh", nodes[i], "-l", "ubuntu"};
-//                    execv("/bin/ssh", exec_arg);
-                    exit(0);
-                } else {
-                    printf("Parent Process My PID:%d, My Parent's PID:%d, MY Childs'Pid %d\n", (int) getpid(),
-                           (int) getppid(),
-                           (int) pid);
-
-                    char *exec_arg[] = {"ssh", nodes[i], "-l", "ubuntu"};
-                    execv("/bin/ssh", exec_arg);
-                    wait(NULL);
-                }
-
-
-            }
-
-            getRemoteCommend(parsing, 3, inputWords, remoteCommand);
-            printf("%s\n", remoteCommand);
-
-        } else if (!strncmp(parsing[1], "--h", strlen(parsing[0]))) {
-
-        }
-
-    }
-    return 0;
-}
-
 int main() {
     pid_t childPid[4] = {0};
     //ssh fork, exec
-    int N = 4;
-    pid_t pid[N]; /* process id */
+    pid_t pid[totalNodesNum]; /* process id */
 
     char buf[MSGSIZE];
     int childStatus;
     int i;
 
-    int fd1[N][2], fd2[N][2];
+    int fd1[totalNodesNum][2], fd2[totalNodesNum][2];
 
     //ssh connect
-    for (i = 0; i < N; i++) {
+    for (i = 0; i < totalNodesNum; i++) {
         //파이프 생성
         if (pipe(fd1[i]) == -1) {
             perror("pipe");
@@ -148,7 +86,7 @@ int main() {
             setvbuf(stdout, NULL, _IOLBF, 0);
 
             //ssh connect  명령어 :  sshpass -p ubuntu ssh -tt node1 -l ubuntu : 비밀번호 추가 입력없이 연결
-            char *exec_arg[] = { "sshpass", "-p", "ubuntu",  "ssh", "-tt", totalNodes[i], "-l", "ubuntu", NULL};
+            char *exec_arg[] = {"sshpass", "-p", "ubuntu", "ssh", "-tt", totalNodes[i], "-l", "ubuntu", NULL};
             execv("/bin/sshpass", exec_arg);
         } else {
             close(fd1[i][0]);
@@ -157,18 +95,19 @@ int main() {
             //buffer setting
             setvbuf(stdin, NULL, _IOLBF, 0);
             setvbuf(stdout, NULL, _IOLBF, 0);
-
-            write(fd1[i][1], "cat /proc/loadavg\n", 19);
         }
-        // Create multiple child processes
     }
 
 
+    //ssh connected 출력
     int nread;
+    bool checkSshConnected[4] = {0};
+    printf("모든 노드에 ssh 연결 중입니다.\n");
     while (1) {
-//        input();
-
-        for (int j = 0; j < N; j++) {
+        for (int j = 0; j < totalNodesNum; j++) {
+            //이미 출력했으면 출력하지 않음
+            if (checkSshConnected[j] == 1) continue;
+            memset(buf, 0, sizeof(buf));
             switch (nread = read(fd2[j][0], buf, MSGSIZE)) {
                 case -1:
                     if (errno == EAGAIN) {
@@ -177,16 +116,115 @@ int main() {
                     } else perror("read call");
                 case 0:
                     printf("End of conversation\n");
-                    exit(0);
+                    break;
                 default:
-                    printf("MSG=%s\n", buf);
+                    checkSshConnected[j] = 1;
+                    printf("%s:%s\n", totalNodes[j], buf);
             }
+        }
+        //모든 노드가 연결되었는지 여부 검사후 while문 종료
+        bool flag = true;
+        for (int i = 0; i < totalNodesNum; i++) {
+            if (!checkSshConnected[i]) {
+                flag = false;
+            }
+        }
+        if (flag) {
+            printf("모든 노드에 연결되었습니다.\n");
+            break;
+        }
+    }
+
+    //명령어 입력
+    while (1) {
+        //input
+        char inputBuf[MSGSIZE] = {0};
+        fgets(inputBuf, sizeof(inputBuf) - 1, stdin);
+        inputBuf[strlen(inputBuf) - 1] = '\0';
+
+        //명령을 전달한 노드가 출력을 완료했는지 여부
+        bool completedNodes[totalNodesNum];
+        for (int i = 0; i < totalNodesNum; i++) {
+            completedNodes[i] = 1;
+        }
+
+        //문자열 파싱
+        char parsing[10][100] = {0};
+        int inputWords = parseInput(inputBuf, parsing, " ");
+
+        //exit
+        if (strlen(inputBuf) != 0 && !strncmp(inputBuf, "exit", strlen(inputBuf))) {
+            printf("exit from cluster shell\n");
+            return -1;
+        } else if (!strncmp(parsing[0], "clsh", strlen(parsing[0]))) { //clsh 명령어
+            char nodes[10][100] = {0};
+            char remoteCommand[200] = {0};
+
+            //clsh -h node1,node2,node3,node4 cat /proc/loadavg
+            if (!strncmp(parsing[1], "-h", strlen(parsing[0]))) {
+                int inputNodesNum = parseInput(parsing[2], nodes, ",");
+                int commandLength = getRemoteCommend(parsing, 3, inputWords, remoteCommand);
+
+                //노드 이름 비교하고 명령 보내기
+                for (int i = 0; i < inputNodesNum; i++) {
+                    //미리 선언한 totalNodes와 이름 비교하고 명령어 전달
+                    for (int j = 0; j < totalNodesNum; j++) {
+                        if (!strncmp(totalNodes[j], nodes[i], strlen(nodes[i]))) {
+                            completedNodes[j] = 0;
+                            write(fd1[j][1], remoteCommand, commandLength);
+                        }
+                    }
+                }
+            }
+        } else {
+            printf("명령어를 다시 입력해주세요.\n");
+            continue;
+        }
+
+
+        while (1) {
+            for (int j = 0; j < totalNodesNum; j++) {
+                //이미 출력했으면 출력하지 않음
+                memset(buf, 0, sizeof(buf));
+                if (completedNodes[j] == 1) continue;
+                switch (nread = read(fd2[j][0], buf, MSGSIZE)) {
+                    case -1:
+                        if (errno == EAGAIN) {
+                            sleep(1);
+                            break;
+                        } else perror("read call");
+                    case 0:
+                        printf("End of conversation\n");
+                        break;
+                    default:
+                        completedNodes[j] = 1;
+                        char procLoadAvg[10][100] = {0};
+
+                        //문자열 파싱
+                        parseInput(buf, procLoadAvg, "\r\n");
+                        printf("%s:%s\n", totalNodes[j], procLoadAvg[2]);
+                }
+            }
+
+            //요청이 모두 출력되었으면 while문 종료
+            bool flag = true;
+            for (int j = 0; j < totalNodesNum; j++) {
+                if (completedNodes[j] == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                break;
+            }
+
+
         }
     }
 
 
     //wait
-    for (i = 0; i < N; i++) {
+    for (i = 0; i < totalNodesNum; i++) {
         pid_t terminatedChild = wait(&childStatus);
         if (WIFEXITED(childStatus)) {
             printf("Child %d has terminated with exit status %d\n", terminatedChild, WEXITSTATUS(childStatus));
